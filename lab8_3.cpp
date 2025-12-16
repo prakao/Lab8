6b492a1f9f83708479d4950b793246a2e0878eba
prakao <kpkrisrin@gmail.com>
2025-12-17 03:50:14 +0700

    lab8_3_finish

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int age,height,bounty;
+string Name_char;
+
+int main(){
+
+    cout<<"Enter your age: ";
+    cin>>age;
+
+    if(age<=25){
+        cout<<"Enter your height: ";
+        cin>>height;
+
+        if(height<100){
+            Name_char = "Chopper";
+        }else if(height<180){
+            Name_char = "Usopp";
+        }else{
+            cout<<"Enter your bounty: ";
+            cin>>bounty;
+
+            if(bounty>1100000000){
+                Name_char = "Zoro";
+            }else{
+                Name_char = "Sanji";
+            }
+        }
+    }
+    else if(age<=60){
+        cout<<"Enter your bounty: ";
+        cin>>bounty;
+
+        if(bounty>500000000){
+            Name_char = "Jinbe";
+        }else{
+            Name_char = "Franky";
+        }
+    }
+    else{
+        Name_char = "Brook";
+    }
+
+    cout<<"Your character = "<<Name_char;
+    return 0;
+}
+
 /*
 "Enter your age: "
 "Enter your height: "
@@ -11,3 +60,4 @@
 "Brook"
 "Jinbe"
 */
+
